8c15604f5bb3103d5c6ddad54d8a894ef80822ff
GuangxingSun <1042445773@qq.com>
2020-03-24 23:09:42 +0800

    更换controller 注解为 RestController

--- src/main/java/com/guangxing/controller/HelloController.java
+++ src/main/java/com/guangxing/controller/HelloController.java
@@ -3,15 +3,15 @@ package com.guangxing.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * hello Controller
  * @author Guangxing
  * @create time 2020/3/24 22:01
  **/
-@Controller
+@RestController
 public class HelloController {
-    @ResponseBody
     @RequestMapping("/hello")
     public String sayHello(){
         return "Hello World!";
